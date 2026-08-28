f13f1a099b28b3a972f7808b0d16f45ec980729c
Bakare Tobiloba <bakareoluwatobiloba@gmail.com>
2026-08-28 23:11:12 +0100

    adding templates

--- src/home/views.py
+++ src/home/views.py
@@ -1,6 +1,22 @@
 from django.http import HttpResponse
+from django.shortcuts import render
+import pathlib
+
+this_dir = pathlib.Path(__file__).resolve().parent
 
 #VIEWS
 def home_page(request, *args, **kwargs):#args: let function take an unknown number of positional arguments, stores them as a tuple.  **\\kwargs* let it take an unknown number of keyword arguments, stores them as a dictionary. useful when you don’t know ahead of time how many values will be passed into your function.
+    my_title = "Tobi"
+    my_context = {
+        "page_title":my_title,
+    }
+    html_template = "home.html"
+    return render(request, html_template, my_context)
 
-    return HttpResponse("<h1> Hello World</h1>")
+
+def home_page_old(request, *args, **kwargs):#args: let function take an unknown number of positional arguments, stores them as a tuple.  **\\kwargs* let it take an unknown number of keyword arguments, stores them as a dictionary. useful when you don’t know ahead of time how many values will be passed into your function.
+    my_title = "Tobi"
+     
+    html_file_path = this_dir / "home.html"
+    html_ = html_file_path.read_text()
+    return HttpResponse(html_) 
